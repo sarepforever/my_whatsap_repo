ef0d0310f6c3ceab76f828028f4a356cea90a5a3
Santiago Perea Restrepo <41804183+sarepforever@users.noreply.github.com>
2024-04-26 11:11:01 -0500

    add
    apppp

---
+++ app.js
@@ -0,0 +1,64 @@
+const { createBot, createProvider, createFlow, addKeyword,EVENTS } = require('@bot-whatsapp/bot')
+const {BaileysProvider, handleCtx} = require('@bot-whatsapp/provider-baileys');
+
+const axios  = require('axios');
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+//const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const UrlFrom="https://zonacute.com/"
+
+const flowPrincipal = addKeyword(EVENTS.WELCOME)
+.addAction(async(ctx)=>{
+    console.log(ctx.from)
+    try{
+        const data ={
+            number:ctx.from,
+            companyId:1,         
+            message:ctx.body,         
+        }
+     
+     const url=`${UrlFrom}api/Chat/receiveMessage`;
+     await axios.post(url,data).then(response=>{
+        console.log(response)
+     });
+     
+    }catch (err){
+        console.log(err)
+    }
+})
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    adapterProvider.initHttpServer(3002);
+    adapterProvider.http?.server.post('/send-message',handleCtx(async (bot,req,res) => {
+        const body = req.body;
+        const phone = req.body.phone;
+        const messagge = req.body.messagge;
+
+        await bot.sendMessage(phone,messagge,{})
+        res.end('Ok')
+    }))
+    adapterProvider.http?.server.post('/send-image',handleCtx(async (bot,req,res) => {
+        const body = req.body;
+        const phone = req.body.phone;
+        const messagge = req.body.messagge;
+        const media = req.body.mediaUrl
+        await bot.sendMessage(phone,messagge,{
+            media
+        })
+        res.end('Ok')
+    }))
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()

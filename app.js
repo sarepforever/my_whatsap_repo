cd85f0847b057144e6522ce09bcae06638892686
Santiago Perea Restrepo <41804183+sarepforever@users.noreply.github.com>
2024-05-07 14:46:54 -0500

    app.js
    commit

--- app.js
+++ app.js
@@ -8,57 +8,62 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
 const UrlFrom="https://zonacute.com/"
+//const UrlFrom="http://localhost:5090/"
 
 const flowPrincipal = addKeyword(EVENTS.WELCOME)
 .addAction(async(ctx)=>{
-    console.log(ctx.from)
+    //console.log(ctx.from)
     try{
         const data ={
             number:ctx.from,
             companyId:1,         
             message:ctx.body,         
         }
-     
+     //console.log(JSON.stringify(ctx))
      const url=`${UrlFrom}api/Chat/receiveMessage`;
      await axios.post(url,data).then(response=>{
-        console.log(response)
+        //console.log(response)
      });
      
     }catch (err){
-        console.log(err)
+       // console.log(err)
     }
 })
 const main = async () => {
-    const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
-    const adapterProvider = createProvider(BaileysProvider)
+    try{
+        const adapterDB = new MockAdapter()
+        const adapterFlow = createFlow([flowPrincipal])
+        const adapterProvider = createProvider(BaileysProvider)
 
-    adapterProvider.initHttpServer(3002);
-    adapterProvider.http?.server.post('/send-message',handleCtx(async (bot,req,res) => {
-        const body = req.body;
-        const phone = req.body.phone;
-        const messagge = req.body.messagge;
+        adapterProvider.initHttpServer(3002);
+        adapterProvider.http?.server.post('/send-message',handleCtx(async (bot,req,res) => {
+            const body = req.body;
+            const phone = req.body.phone;
+            const messagge = req.body.messagge;
 
-        await bot.sendMessage(phone,messagge,{})
-        res.end('Ok')
-    }))
-    adapterProvider.http?.server.post('/send-image',handleCtx(async (bot,req,res) => {
-        const body = req.body;
-        const phone = req.body.phone;
-        const messagge = req.body.messagge;
-        const media = req.body.mediaUrl
-        await bot.sendMessage(phone,messagge,{
-            media
+            await bot.sendMessage(phone,messagge,{})
+            res.end('Ok')
+        }))
+        adapterProvider.http?.server.post('/send-image',handleCtx(async (bot,req,res) => {
+            const body = req.body;
+            const phone = req.body.phone;
+            const messagge = req.body.messagge;
+            const media = req.body.mediaUrl
+            await bot.sendMessage(phone,messagge,{
+                media
+            })
+            res.end('Ok')
+        }))
+        createBot({
+            flow: adapterFlow,
+            provider: adapterProvider,
+            database: adapterDB,
         })
-        res.end('Ok')
-    }))
-    createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
 
-    QRPortalWeb()
+        QRPortalWeb()
+    }catch{
+
+    }
 }
 
 main()
